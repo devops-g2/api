4447e66cc480fc90299460d9a964724a67a66467
emanueljg <emanueljohnsongodin@gmail.com>
2023-09-07 21:02:57 +0200

    do some work on /tldr route

--- api/main.py
+++ api/main.py
@@ -5,15 +5,15 @@ from typing import Annotated
 from fastapi import Depends, FastAPI, HTTPException
 from fastapi.responses import PlainTextResponse
 from fastapi_crudrouter import SQLAlchemyCRUDRouter
-from prometheus_fastapi_instrumentator import Instrumentator
 from sqlmodel import Session, select
+import textwrap
+
+# from .models import *
 
 from .models import MODELS, Category, Index, Post, Thread, create_all, get_db
-from .tldr import tldr_docs
+# from .tldr import tldr_docs
 
 app = FastAPI()
-
-Instrumentator().instrument(app).expose(app, include_in_schema=True, should_gzip=True)
 
 
 # generate CRUD routes
@@ -91,9 +91,78 @@ def get_thread_post_ids(
     ).all()
 
 
+endpoint_str = textwrap.dedent('''
+
+    Endpoints: Special
+    ------------------
+
+    GET /categories/<category_id>/threads
+        Get a list of a category's thread IDs.
+
+    GET /threads/<thread_id>/posts
+        Get a list of a thread's post IDs.
+
+
+    Endpoints: Generic CRUDs supported
+    ----------------------------------
+
+    GET /<items> (?offset=0&limit=100)
+        Get a list of items.
+
+    POST /<items>
+        Create an item.
+
+    GET /<items>/<item_id>
+        Get an item.
+
+    UPDATE /<items>/<item_id>
+        Update an item.
+
+    DELETE /<items>
+        Delete all items.
+
+    DELETE /<items>/<item_id>
+        Delete an item.
+''').lstrip()
+
+print(dir(app))
+print(app.openapi_tags)
+
+
 @app.get("/tldr", response_class=PlainTextResponse)
 async def get_tldr_docs() -> str:
-    return tldr_docs
+    return str(dir(app))
+    # return str(app.openapi())
+    lines_mixed_types = [
+        'Objects',
+        '-------',
+        ''
+    ]
+    for model_name, model in MODELS.items():
+        schema = model.table.schema()
+        lines_mixed_types.append(f'{schema["title"]}:  (/{model.prefix})')
+        for property_name, property in schema['properties'].items():
+            lines_mixed_types.append({
+                'string': f'{" " * 4}{property_name}: {property["type"]}',
+                'is_readonly': property_name not in model.creator.schema()['properties']
+            })
+        lines_mixed_types.append('\n')
+
+    lines = []
+
+    for line in lines_mixed_types:
+        if not isinstance(line, dict): 
+            lines.append(line)
+        elif not line['is_readonly']:   # 
+            lines.append(line['string'])
+        else:
+            readonly_tag_index = len(max(lines_mixed_types, key=len)) + 2
+            lines.append(f'{line["string"]: <{readonly_tag_index}}(read-only)')
+
+    return '\n'.join(lines) + '\n' + endpoint_str
+
+
+    # return tldr_docs
 
 
 if __name__ == "__main__":
