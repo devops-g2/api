73e5fe4e574d53fab5fe6888d31ffde1670ccb93
emanueljg <emanueljohnsongodin@gmail.com>
2024-01-19 11:06:34 +0100

    add click

--- api/main.py
+++ api/main.py
@@ -7,6 +7,8 @@ from fastapi.middleware.cors import CORSMiddleware
 from sqlmodel import create_engine
 
 from .models import Endpointer
+import uvicorn
+import click
 
 app = FastAPI()
 
@@ -21,24 +23,24 @@ app.add_middleware(
 )
 
 
-DB = Path("forum.db")
-
-# prod
-ENGINE = create_engine(f"sqlite:///{DB}", echo=True)
-
-# echo SQL statements sent; useful for debugging
-
-
-@app.on_event("startup")
-def on_startup() -> None:
-    DB.unlink(missing_ok=True)
-    Endpointer.init(ENGINE, do_seed=False)
-
-
 @app.get("/")
 async def root() -> dict[str, str]:
     return {"msg": "Hello World"}
 
 
+@click.command()
+@click.option('--seed/--no-seed', default=False)
+@click.option('--db-path', required=True)
+@click.option('--db-echo/--no-db-echo', default=False)
+@click.option('--db-wipe-on-start/--no-db-wipe-on-start', default=False)
+def start(seed, db_path, db_echo, db_wipe_on_start):
+    db = Path(db_path)
+    engine = create_engine(f"sqlite:///{db}", echo=db_echo)
+    if db_wipe_on_start:
+        db.unlink(missing_ok=True)
+    Endpointer.init(engine, do_seed=seed)
+    uvicorn.run(app, port=8000)
+
+
 if __name__ == "__main__":
-    pass
+    start()
